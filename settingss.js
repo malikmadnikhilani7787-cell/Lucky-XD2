3114de4dd8b81877d2cd32130203daeb9be4d881
malikmadnikhilani7787-cell <malikmadnikhilani7787@gmail.com>
2025-09-21 13:14:59 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
-  OWNER_NUMBER: "", // put your phone number here
+  SESSION_ID: "malvin~Bu1X3KRa#92QqQQVaAX2T6wMjMTgaEBmkc4gMBGcdRn6YcV2SFpc",  // add your Session Id here
+  OWNER_NUMBER: "15167559026", // put your phone number here
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   TIMEZONE: "Africa/Kampala" //put your country timeZone....leave blank if u don't know.
 };
